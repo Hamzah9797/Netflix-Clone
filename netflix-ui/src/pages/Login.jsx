861e60ee29030dd0edec3c1e5f6e152e446980ab
Hamzah9797 <hamzahhameed9797@gmail.com>
2023-08-12 12:29:46 +0700

    login page

--- netflix-ui/src/pages/Login.jsx
+++ netflix-ui/src/pages/Login.jsx
@@ -1,7 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 15vh 85vh;
+    .form-container {
+      gap: 2rem;
+      height: 85vh;
+      .form {
+        gap: 2rem;
+        background-color: #000000b0;
+        width: 25vw;
+        gap: 2rem;
+        color: #fff;
+        .container {
+          gap: 2rem;
+          input {
+            padding: 0.5rem 1rem;
+            width: 15rem;
+          }
+          button {
+            padding: 0.5rem 1rem;
+            background-color: #e50914;
+            border: none;
+            cursor: pointer;
+            color: #fff;
+            border-radius: 0.2rem;
+            font-weight: bolder;
+            font-size: 1.05rem;
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const { email, password } = formValues;
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) navigate("/");
+  });
+
+  return (
+    <Container>
+      <BackgroundImage />
+      <div className="content">
+        <Header />
+        <div className="form-container flex column a-center j-center">
+          <div className="form flex column a-center j-center">
+            <div className="title">
+              <h3>Login</h3>
+              <div className="container flex column">
+                <input
+                  type="email"
+                  placeholder="email address"
+                  name="email"
+                  value={formValues.email}
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, email: e.target.value })
+                  }
+                />
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  value={formValues.password}
+                  onChange={(e) =>
+                    setFormValues({ ...formValues, password: e.target.value })
+                  }
+                />
+                <button onClick={handleLogin}>Log In</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Login;
