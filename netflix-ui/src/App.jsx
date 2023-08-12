a70a9cd652e5bbc948c9c351bb8bcb07bd2dcd34
Hamzah9797 <hamzahhameed9797@gmail.com>
2023-08-12 16:12:59 +0700

    done with player

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Netflix from "./pages/Netflix";
 import SignUp from "./pages/SignUp";
+import Player from "./pages/Player";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/" element={<Netflix />} />
+        <Route exact path="/player" element={<Player />} />
       </Routes>
     </BrowserRouter>
   );
